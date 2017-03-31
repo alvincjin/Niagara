11586bac65f971e44ba909a12ed6716521f3490d
alvinjin <alvincjin@gmail.com>
2017-03-30 22:46:16 -0400

    Added data model for restaurant

--- build.sbt
+++ build.sbt
@@ -59,7 +59,8 @@ libraryDependencies ++= Seq(
   "com.github.t3hnar" %% "scala-bcrypt" % "2.4",
   "com.jason-goodwin" %% "authentikat-jwt" % "0.4.3",
   "com.github.nscala-time" %% "nscala-time" % "2.4.0",
-  "com.danielasfregola" %% "twitter4s" % "5.0"
+  "com.danielasfregola" %% "twitter4s" % "5.0",
+  "org.reactivemongo" %% "reactivemongo" % "0.12"
 )
 
 assemblyJarName in assembly := s"${name.value}-${version.value}.jar"
--- src/main/scala/com/alvin/niagara/twitter/UserTopHashTags.scala
+++ src/main/scala/com/alvin/niagara/twitter/UserTopHashTags.scala
@@ -15,10 +15,22 @@ object UserTopHashTags extends App with Config {
   def getTopHashtags(tweets: Seq[Tweet], n: Int = 10): Seq[(String, Int)] = {
 
     val hashtags: Seq[Seq[HashTag]] = tweets.map { tweet =>
+
+      //println(tweet.text.toString)
       tweet.entities.map(_.hashtags).getOrElse(Seq.empty)
     }
 
+    //"@TryCaviar" or "caviar"
+    //tweets.filter(_.text.contains("@TryCaviar")).map(x => println(x.id + "  "+x.created_at.toString  +" : " +x.text))
+    tweets.map { tweet => println(tweet.id + "  " + tweet.created_at.toString + " : " + tweet.text)
+               tweet.entities.map(_.hashtags).getOrElse(Seq.empty).map(_.text.toLowerCase).foreach(println)
+      println("#############")
+      tweet.entities.map(_.user_mentions).getOrElse(Seq.empty).map(_.screen_name.toLowerCase).foreach(println)
+    }
+
     val hashtagTexts: Seq[String] = hashtags.flatten.map(_.text.toLowerCase)
+
+    hashtagTexts.foreach(println)
 
     val hashtagFrequencies: Map[String, Int] = hashtagTexts.groupBy(identity).mapValues(_.size)
 
@@ -31,9 +43,11 @@ object UserTopHashTags extends App with Config {
 
   val restClient = TwitterRestClient(consumerToken, accessToken)
 
-  val user = "odersky"
+  val user = "realDonaldTrump"//"KimKardashian"
 
-  val result = restClient.userTimelineForUser(screen_name = user, count = 200)
+  val since_id = None//Some(814147150330228736L)
+
+  val result = restClient.userTimelineForUser(screen_name = user, since_id = since_id, count = 1000)
     .map { ratedData =>
 
       val tweets = ratedData.data
